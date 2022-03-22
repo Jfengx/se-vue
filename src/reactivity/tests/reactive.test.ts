b94c64ba625415991473d869e824e3961c623878
JiaFeng <18800295187@163.com>
2022-03-22 14:56:41 +0800

    feat: reactive

---
+++ src/reactivity/tests/reactive.test.ts
@@ -0,0 +1,12 @@
+import { reactive } from '../reactive';
+
+describe('reactive', () => {
+  it('happy path', () => {
+    const original = { foo: 1 };
+    const observed = reactive(original);
+
+    // 不相等
+    expect(observed).not.toBe(original);
+    expect(observed.foo).toBe(1);
+  });
+});
